9ec220b64b356297cd7a93731cccb0f5db401fd1
Nathan Rajlich <nathan@tootallnate.net>
2017-10-19 12:10:01 -0700

    implement it
    Closes #1.

---
+++ index.js
@@ -0,0 +1,34 @@
+const { isIP } = require('net');
+const { format, parse } = require('url');
+const resolve = require('@zeit/dns-cached-resolve');
+
+module.exports = setup;
+
+function setup (fetch) {
+  if (!fetch) {
+    fetch = require('node-fetch');
+  }
+
+  async function fetchCachedDns(url, opts) {
+    const parsed = parse(url);
+    const ip = isIP(parsed.hostname);
+    if (ip === 0) {
+      if (!opts) opts = {};
+      if (!opts.headers) opts.headers = {};
+      if (!opts.headers.Host) {
+        opts.headers.Host = parsed.host;
+      }
+      parsed.host = await resolve(parsed.hostname);
+      url = format(parsed);
+    }
+    return fetch(url, opts);
+  }
+
+  for (const key of Object.keys(fetch)) {
+    fetchCachedDns[key] = fetch[key];
+  }
+
+  fetchCachedDns.default = fetchCachedDns;
+
+  return fetchCachedDns;
+}
