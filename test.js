fe38dde0ba676a67b42dfcc2dff1db1c0254c56d
Guillermo Rauch <rauchg@gmail.com>
2017-11-03 16:23:26 -0700

    pass linting

--- test.js
+++ test.js
@@ -1,29 +1,35 @@
-const {createServer} =  require('http');
-const fetch = require('./index')();
+/* eslint-env jest */
+const { createServer } = require('http')
+const fetch = require('./index')()
 
 test('retries upon 500', async () => {
   let i = 0
   const server = createServer((req, res) => {
     if (i++ < 2) {
-      res.writeHead(500);
-      res.end();
+      res.writeHead(500)
+      res.end()
     } else {
-      res.end('ha');
+      res.end('ha')
     }
-  });
+  })
 
   return new Promise((resolve, reject) => {
     server.listen(async () => {
-      const {port} = server.address();
+      const { port } = server.address()
       try {
-        const res = await fetch(`http://127.0.0.1:${port}`);
-        expect(await res.text()).toBe('ha');
-        server.close();
-        resolve();
+        const res = await fetch(`http://127.0.0.1:${port}`)
+        expect(await res.text()).toBe('ha')
+        server.close()
+        resolve()
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
-    server.on('error', reject);
-  });
-});
+    })
+    server.on('error', reject)
+  })
+})
+
+test('works with https', async () => {
+  const res = await fetch('https://zeit.co')
+  expect(await res.text()).toContain('Now')
+})
