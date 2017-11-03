ad0d3026279495f42fb209ee3752410d7cb0c9cb
Guillermo Rauch <rauchg@gmail.com>
2017-11-03 16:23:44 -0700

    add support for http and https pools, add debugging

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
-const { HttpsAgent } = require('agentkeepalive');
-const setupFetchRetry = require('@zeit/fetch-retry');
-const setupFetchCachedDns = require('@zeit/fetch-cached-dns');
+const HttpAgent = require('agentkeepalive')
+const { HttpsAgent } = require('agentkeepalive')
+const setupFetchRetry = require('@zeit/fetch-retry')
+const setupFetchCachedDns = require('@zeit/fetch-cached-dns')
+const debug = require('debug')('@zeit/fetch')
 
-module.exports = setup;
+module.exports = setup
 
-let defaultGlobalAgent;
+const AGENT_OPTS = {
+  maxSockets: 200,
+  maxFreeSockets: 20,
+  timeout: 60000,
+  freeSocketKeepAliveTimeout: 30000 // free socket keepalive for 30 seconds
+}
 
-function getDefaultGlobalAgent() {
-  return defaultGlobalAgent = defaultGlobalAgent || new HttpsAgent({
-    maxSockets: 200,
-    maxFreeSockets: 20,
-    timeout: 60000,
-    freeSocketKeepAliveTimeout: 30000 // free socket keepalive for 30 seconds
-  });
+let defaultHttpGlobalAgent
+let defaultHttpsGlobalAgent
+
+function getDefaultHttpGlobalAgent() {
+  return (defaultHttpGlobalAgent =
+    defaultHttpGlobalAgent ||
+    (debug('init http agent'), new HttpAgent(AGENT_OPTS)))
+}
+
+function getDefaultHttpsGlobalAgent() {
+  return (defaultHttpsGlobalAgent =
+    defaultHttpsGlobalAgent ||
+    (debug('init https agent'), new HttpsAgent(AGENT_OPTS)))
 }
 
 function wrapDefaultGlobalAgent(fetch) {
   return /* async */ function fetchWithGlobalAgent(url, opts = {}, ...args) {
-    opts.agent = opts.agent || getDefaultGlobalAgent();
-    return fetch(url, opts, ...args);
-  };
+    opts.agent =
+      opts.agent ||
+      (/^https/.test(url)
+        ? getDefaultHttpsGlobalAgent()
+        : getDefaultHttpGlobalAgent())
+    return fetch(url, opts, ...args)
+  }
 }
 
 function setup(fetch) {
   if (!fetch) {
-    fetch = require('node-fetch');
+    fetch = require('node-fetch')
   }
-  fetch = setupFetchCachedDns(fetch);
-  fetch = setupFetchRetry(fetch);
-  fetch = wrapDefaultGlobalAgent(fetch);
-  return fetch;
+  fetch = setupFetchCachedDns(fetch)
+  fetch = setupFetchRetry(fetch)
+  fetch = wrapDefaultGlobalAgent(fetch)
+  return fetch
 }
