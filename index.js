f524baeb25d33838a55cae6934a173e3fa3a151b
Nathan Rajlich <nathan@tootallnate.net>
2017-11-16 13:09:22 -0800

    fix redirects to a different hostname
    Previously the explicit Host header would cause issues if
    the redirect is a different hostname.
    
    Test case included.

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-const { isIP } = require('net');
-const { format, parse } = require('url');
-const resolve = require('@zeit/dns-cached-resolve');
+const { isIP } = require('net')
+const { format, parse } = require('url')
+const resolve = require('@zeit/dns-cached-resolve')
 
-module.exports = setup;
+module.exports = setup
 
-function setup (fetch) {
+const isRedirect = v => ((v / 100) | 0) === 3
+
+function setup(fetch) {
   if (!fetch) {
-    fetch = require('node-fetch');
+    fetch = require('node-fetch')
   }
+  const { Headers } = fetch
 
   async function fetchCachedDns(url, opts) {
-    const parsed = parse(url);
-    const ip = isIP(parsed.hostname);
+    const parsed = parse(url)
+    const ip = isIP(parsed.hostname)
     if (ip === 0) {
-      if (!opts) opts = {};
-      if (!opts.headers) opts.headers = {};
-      if (!opts.headers.Host) {
-        opts.headers.Host = parsed.host;
+      if (!opts) opts = {}
+      if (!opts.headers) opts.headers = new Headers()
+      if (!opts.headers.has('Host')) {
+        opts.headers.set('Host', parsed.host)
       }
-      parsed.hostname = await resolve(parsed.hostname);
-      url = format(parsed);
+      opts.redirect = 'manual'
+      parsed.hostname = await resolve(parsed.hostname)
+      url = format(parsed)
     }
-    return fetch(url, opts);
+    const res = await fetch(url, opts)
+    if (isRedirect(res.status)) {
+      const redirectOpts = Object.assign({}, opts)
+      if (!redirectOpts.headers) redirectOpts.headers = new Headers()
+
+      // per fetch spec, for POST request with 301/302 response, or any request with 303 response, use GET when following redirect
+      if (
+        res.status === 303 ||
+        ((res.status === 301 || res.status === 302) && opts.method === 'POST')
+      ) {
+        redirectOpts.method = 'GET'
+        redirectOpts.body = null
+        redirectOpts.headers.delete('content-length')
+      }
+
+      const location = res.headers.get('Location')
+      redirectOpts.headers.delete('Host')
+      return fetchCachedDns(location, redirectOpts)
+    } else {
+      return res
+    }
   }
 
   for (const key of Object.keys(fetch)) {
-    fetchCachedDns[key] = fetch[key];
+    fetchCachedDns[key] = fetch[key]
   }
 
-  fetchCachedDns.default = fetchCachedDns;
+  fetchCachedDns.default = fetchCachedDns
 
-  return fetchCachedDns;
+  return fetchCachedDns
 }
--- test.js
+++ test.js
@@ -1,24 +1,49 @@
 /* eslint-env jest*/
+const listen = require('async-listen')
 const { createServer } = require('http')
-const cachedDNSFetch = require('./index')()
+const cachedDNSFetch = require('./index')(require('node-fetch'))
 
-test('works with localhost', async () => {
+/**
+ * Using `localtest.me` to use DNS to resolve to localhost
+ * http://readme.localtest.me/
+ */
+
+test('works with localtest.me', async () => {
   const server = createServer((req, res) => {
     res.end('ha')
   })
 
-  return new Promise((resolve, reject) => {
-    server.listen(async () => {
-      const { port } = server.address()
-      try {
-        const res = await cachedDNSFetch(`http://127.0.0.1:${port}`)
-        expect(await res.text()).toBe('ha')
-        server.close()
-        resolve()
-      } catch (err) {
-        reject(err)
-      }
-    })
-    server.on('error', reject)
+  await listen(server)
+  const { port } = server.address()
+
+  const res = await cachedDNSFetch(`http://localtest.me:${port}`)
+  expect(await res.text()).toBe('ha')
+  server.close()
+})
+
+test('works with redirects', async () => {
+  let portA
+  let portB
+
+  const serverA = createServer((req, res) => {
+    res.setHeader('Location', `http://localtest.me:${portB}`)
+    res.statusCode = 302
+    res.end()
   })
+  const serverB = createServer((req, res) => {
+    // ensure the Host header is properly re-written upon redirect
+    res.end(req.headers.host)
+  })
+
+  await listen(serverA)
+  await listen(serverB)
+  portA = serverA.address().port
+  portB = serverB.address().port
+
+  const res = await cachedDNSFetch(`http://localtest.me:${portA}`)
+  expect(await res.status).toBe(200)
+  expect(await res.text()).toBe(`localtest.me:${portB}`)
+
+  serverA.close()
+  serverB.close()
 })
