fdde59804b7d93c2d3983bb4ca499715516cc6b6
Nathan Rajlich <nathan@tootallnate.net>
2018-04-30 16:20:12 -0700

    Fix lint

--- index.js
+++ index.js
@@ -1,83 +1,83 @@
-const { parse: parseUrl } = require('url')
-const HttpAgent = require('agentkeepalive')
-const debug = require('debug')('@zeit/fetch')
-const setupFetchRetry = require('@zeit/fetch-retry')
-const setupFetchCachedDns = require('@zeit/fetch-cached-dns')
+const {parse: parseUrl} = require('url');
+const HttpAgent = require('agentkeepalive');
+const debug = require('debug')('@zeit/fetch');
+const setupFetchRetry = require('@zeit/fetch-retry');
+const setupFetchCachedDns = require('@zeit/fetch-cached-dns');
 
-const { HttpsAgent } = HttpAgent
+const {HttpsAgent} = HttpAgent;
 
 const AGENT_OPTS = {
 	maxSockets: 200,
 	maxFreeSockets: 20,
 	timeout: 60000,
 	freeSocketKeepAliveTimeout: 30000 // free socket keepalive for 30 seconds
-}
+};
 
-let defaultHttpGlobalAgent
-let defaultHttpsGlobalAgent
+let defaultHttpGlobalAgent;
+let defaultHttpsGlobalAgent;
 
 function getDefaultHttpGlobalAgent() {
 	return (defaultHttpGlobalAgent =
 		defaultHttpGlobalAgent ||
-		(debug('init http agent'), new HttpAgent(AGENT_OPTS)))
+		(debug('init http agent'), new HttpAgent(AGENT_OPTS)));
 }
 
 function getDefaultHttpsGlobalAgent() {
 	return (defaultHttpsGlobalAgent =
 		defaultHttpsGlobalAgent ||
-		(debug('init https agent'), new HttpsAgent(AGENT_OPTS)))
+		(debug('init https agent'), new HttpsAgent(AGENT_OPTS)));
 }
 
 function getAgent(url) {
 	return /^https/.test(url)
 		? getDefaultHttpsGlobalAgent()
-		: getDefaultHttpGlobalAgent()
+		: getDefaultHttpGlobalAgent();
 }
 
 function setupZeitFetch(fetch) {
 	return function zeitFetch(url, opts = {}) {
 		if (!opts.agent) {
 			// Add default `agent` if none was provided
-			opts.agent = getAgent(url)
+			opts.agent = getAgent(url);
 		}
 
-		opts.redirect = 'manual'
-		opts.headers = new fetch.Headers(opts.headers)
+		opts.redirect = 'manual';
+		opts.headers = new fetch.Headers(opts.headers);
 		// Workaround for node-fetch + agentkeepalive bug/issue
-		opts.headers.set('host', opts.headers.get('host') || parseUrl(url).host)
+		opts.headers.set('host', opts.headers.get('host') || parseUrl(url).host);
 
 		// Convert Object bodies to JSON
 		if (opts.body && typeof opts.body === 'object') {
-			opts.body = JSON.stringify(opts.body)
-			opts.headers.set('Content-Type', 'application/json')
-			opts.headers.set('Content-Length', Buffer.byteLength(opts.body))
+			opts.body = JSON.stringify(opts.body);
+			opts.headers.set('Content-Type', 'application/json');
+			opts.headers.set('Content-Length', Buffer.byteLength(opts.body));
 		}
 
 		// Check the agent on redirections
 		opts.onRedirect = (res, redirectOpts) => {
-			redirectOpts.agent = getAgent(res.headers.get('Location'))
-		}
+			redirectOpts.agent = getAgent(res.headers.get('Location'));
+		};
 
-		debug('%s %s', opts.method || 'GET', url)
-		return fetch(url, opts)
-	}
+		debug('%s %s', opts.method || 'GET', url);
+		return fetch(url, opts);
+	};
 }
 
 function setup(fetch) {
 	if (!fetch) {
-		fetch = require('node-fetch')
+		fetch = require('node-fetch');
 	}
 
 	if (typeof fetch !== 'function') {
 		throw new Error(
 			"fetch() argument isn't a function; did you forget to initialize your @zeit/fetch import?"
-		)
+		);
 	}
 
-	fetch = setupFetchCachedDns(fetch)
-	fetch = setupFetchRetry(fetch)
-	fetch = setupZeitFetch(fetch)
-	return fetch
+	fetch = setupFetchCachedDns(fetch);
+	fetch = setupFetchRetry(fetch);
+	fetch = setupZeitFetch(fetch);
+	return fetch;
 }
 
-module.exports = setup
+module.exports = setup;
--- test/index.js
+++ test/index.js
@@ -1,49 +1,49 @@
-const { createServer } = require('http')
-const fetch = require('../index')()
+const {createServer} = require('http');
+const fetch = require('../index')();
 
 exports.retriesUponHttp500 = () => {
-	let i = 0
+	let i = 0;
 	const server = createServer((req, res) => {
 		if (i++ < 2) {
-			res.writeHead(500)
-			res.end()
+			res.writeHead(500);
+			res.end();
 		} else {
-			res.end('ha')
+			res.end('ha');
 		}
-	})
+	});
 
 	return new Promise((resolve, reject) => {
 		server.listen(async () => {
-			const { port } = server.address()
+			const {port} = server.address();
 			try {
-				const res = await fetch(`http://127.0.0.1:${port}`)
-				const resBody = await res.text()
-				server.close()
+				const res = await fetch(`http://127.0.0.1:${port}`);
+				const resBody = await res.text();
+				server.close();
 				if (resBody === 'ha') {
-					resolve()
+					resolve();
 				} else {
-					reject(resBody)
+					reject(resBody);
 				}
 			} catch (err) {
-				reject(err)
+				reject(err);
 			}
-		})
-		server.on('error', reject)
-	})
-}
+		});
+		server.on('error', reject);
+	});
+};
 
 exports.worksWithHttps = async () => {
-	const res = await fetch('https://zeit.co')
-	const resText = await res.text()
+	const res = await fetch('https://zeit.co');
+	const resText = await res.text();
 
 	if (!resText.includes('Now')) {
-		throw new Error("Doesn't work with https")
+		throw new Error("Doesn't work with https");
 	}
-}
+};
 
 /**
  * We know that http://zeit.co redirects to https so we can use it
  * as a test to make sure that we switch the agent when the it
  * happens
  */
-exports.switchesAgentsOnRedirect = async () => fetch(`http://zeit.co`)
+exports.switchesAgentsOnRedirect = async () => fetch(`http://zeit.co`);
