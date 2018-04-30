abc437b7ee383087dba1d3349a3f8605f3c2068d
Nathan Rajlich <nathan@tootallnate.net>
2018-04-30 16:13:35 -0700

    Update to latest `@zeit/eslint-config-node` and add editorconfig

--- test/index.js
+++ test/index.js
@@ -2,43 +2,43 @@ const { createServer } = require('http')
 const fetch = require('../index')()
 
 exports.retriesUponHttp500 = () => {
-  let i = 0
-  const server = createServer((req, res) => {
-    if (i++ < 2) {
-      res.writeHead(500)
-      res.end()
-    } else {
-      res.end('ha')
-    }
-  })
+	let i = 0
+	const server = createServer((req, res) => {
+		if (i++ < 2) {
+			res.writeHead(500)
+			res.end()
+		} else {
+			res.end('ha')
+		}
+	})
 
-  return new Promise((resolve, reject) => {
-    server.listen(async () => {
-      const { port } = server.address()
-      try {
-        const res = await fetch(`http://127.0.0.1:${port}`)
-        const resBody = await res.text()
-        server.close()
-        if (resBody === 'ha') {
-          resolve()
-        } else {
-          reject(resBody)
-        }
-      } catch (err) {
-        reject(err)
-      }
-    })
-    server.on('error', reject)
-  })
+	return new Promise((resolve, reject) => {
+		server.listen(async () => {
+			const { port } = server.address()
+			try {
+				const res = await fetch(`http://127.0.0.1:${port}`)
+				const resBody = await res.text()
+				server.close()
+				if (resBody === 'ha') {
+					resolve()
+				} else {
+					reject(resBody)
+				}
+			} catch (err) {
+				reject(err)
+			}
+		})
+		server.on('error', reject)
+	})
 }
 
 exports.worksWithHttps = async () => {
-  const res = await fetch('https://zeit.co')
-  const resText = await res.text()
+	const res = await fetch('https://zeit.co')
+	const resText = await res.text()
 
-  if (!resText.includes('Now')) {
-    throw new Error("Doesn't work with https")
-  }
+	if (!resText.includes('Now')) {
+		throw new Error("Doesn't work with https")
+	}
 }
 
 /**
@@ -46,6 +46,4 @@ exports.worksWithHttps = async () => {
  * as a test to make sure that we switch the agent when the it
  * happens
  */
-exports.switchesAgentsOnRedirect = async () => {
-  return fetch(`http://zeit.co`)
-}
+exports.switchesAgentsOnRedirect = async () => fetch(`http://zeit.co`)
