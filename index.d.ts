e23afed79937b3157dc7cc2b6fa2b43da13eb540
Olli Vanhoja <olli.vanhoja@gmail.com>
2019-02-21 12:19:26 +0100

    Fix broken TS definition (#29)
    * Fix broken TS definition
    * Add agent to the typing
    * Add types for async-retry options

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,16 @@
-import Fetch from 'node-fetch';
+import { Agent } from 'http';
+import { Options as RetryOptions } from 'async-retry';
+import { Request, RequestInit, Response } from 'node-fetch';
+
+export type FetchOptions = RequestInit & {
+	agent?: Agent
+	retry?: RetryOptions
+}
+
+export type Fetch = (
+	url: string | Request,
+	options?: FetchOptions
+) => Promise<Response>;
+
 export default function SetupFetch(client?: Fetch): Fetch;
+export * from 'node-fetch';
