2565cdbd1e24e52e063de8385a2ef5e39f40029f
Olli Vanhoja <olli.vanhoja@gmail.com>
2019-02-21 13:10:38 +0200

    Add agent to the typing

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
+import { Agent } from 'http';
 import { Request, RequestInit, Response } from 'node-fetch';
 
 type FetchFn = (
 	url: string | Request,
-	init?: RequestInit & { retry: object}) => Promise<Response>;
+	init?: RequestInit & { agent?: Agent, retry: object}) => Promise<Response>;
 
 export * from 'node-fetch';
 export default function SetupFetch(client?: FetchFn): FetchFn;
