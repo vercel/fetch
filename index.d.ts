e4ee94c0076683bc33e4ffeca4f8062ab5806ec6
Raymond Huang <raymond.huang@unbounce.com>
2020-01-07 13:39:09 -0800

    Change TypeScript import to match module definition

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import * as http from 'http';
 import * as https from 'http';
 import { Options as RetryOptions } from 'async-retry';
-import { Request, RequestInit, Response } from 'node-fetch';
+import { Headers, Request, RequestInit, Response } from 'node-fetch';
 
 export type FetchOptions = RequestInit & {
 	agent?: https.Agent | http.Agent
@@ -13,5 +13,14 @@ export type Fetch = (
 	options?: FetchOptions
 ) => Promise<Response>;
 
-export default function SetupFetch(client?: Fetch, agentOptions?: http.AgentOptions | https.AgentOptions): Fetch;
+export type FetchModule = {
+	default: Fetch;
+	Headers: Headers;
+}
+
+export default function SetupFetch(
+	fetchModule?: FetchModule,
+	agentOptions?: http.AgentOptions | https.AgentOptions
+): Fetch;
+
 export * from 'node-fetch';
