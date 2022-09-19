d71ff66c4afd9168e8529c414722a20468f88754
Felix Haus <ofhouse@users.noreply.github.com>
2022-09-19 20:08:12 +0200

    types: Add optional fetch param (#70)

--- packages/fetch-cached-dns/index.d.ts
+++ packages/fetch-cached-dns/index.d.ts
@@ -1,6 +1,6 @@
-import { Request, RequestInit, Response } from 'node-fetch';
+import NodeFetch, { Request, RequestInit, Response } from 'node-fetch';
 
-export default function createFetch(): (
+export default function createFetch(fetch?: typeof NodeFetch): (
   url: string | Request,
   init?: RequestInit,
 ) => Promise<Response>;
